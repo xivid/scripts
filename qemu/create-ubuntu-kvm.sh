62f25eaa23054e61cffe259e4bf8895a5186ffa8
Zhifei Yang <xividyzf@gmail.com>
2022-09-09 17:39:59 +0200

    update

--- qemu/create-ubuntu-kvm.sh
+++ qemu/create-ubuntu-kvm.sh
@@ -8,6 +8,8 @@ if [ ! -f ubuntu.img ]; then
 	echo "Resize the image to a larger size..."
 	echo "===================================="
 	qemu-img resize ubuntu.img +100G
+	# If the VM has the "cloud-initramfs-growroot" package pre-installed (which is our case),
+	# its root partition will be expanded upon next startup.
 fi
 
 if [ ! -f user-data.img ]; then
@@ -23,6 +25,13 @@ EOF
 	
 	cloud-localds user-data.img user-data
 	rm user-data
+
+	echo "Config created! Now you can start the VM with run-vm.sh, and ssh into it with ubuntu:ubuntu.
 fi
 
-
+# To allow ssh into the root:
+# Change the /etc/ssh/sshd_config file to:
+#
+# #Authentication
+# PermitRootLogin yes
+#
